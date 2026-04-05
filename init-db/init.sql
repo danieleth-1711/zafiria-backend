366baf78b384bca33bd56e7f17da911cf9719886
Danieleth <danieleth.lafu@gmail.com>
2026-04-05 19:30:54 -0400

    Actualiza compose.yaml con frontend y script SQL mejorado

--- init-db/init.sql
+++ init-db/init.sql
@@ -1,1 +1,16 @@
+-- Crear base para Keycloak
 CREATE DATABASE keycloak_db;
+
+-- Conectarse a keycloak_db
+\c keycloak_db
+
+GRANT ALL PRIVILEGES ON DATABASE keycloak_db TO zafiria_user;
+GRANT ALL ON SCHEMA public TO zafiria_user;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO zafiria_user;
+
+-- Conectarse a la base de Zafiria creada por docker
+\c zafiria_db
+
+GRANT ALL PRIVILEGES ON DATABASE zafiria_db TO zafiria_user;
+GRANT ALL ON SCHEMA public TO zafiria_user;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO zafiria_user;
